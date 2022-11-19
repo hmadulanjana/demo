ecaea587b0d5928e13581513d57ca6de5f654c9e
Akila Dulanjana <akiladulanjana100@gmail.com>
2022-11-19 12:42:01 +0530

    Add migration with FK support

--- src/database/migrations/2022_11_19_051902_create_ticket_types_table.php
+++ src/database/migrations/2022_11_19_051902_create_ticket_types_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
     {
         Schema::create('ticket_types', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name');
+            $table->string('color');
         });
     }
 
--- src/database/migrations/2022_11_19_051918_create_tickets_table.php
+++ src/database/migrations/2022_11_19_051918_create_tickets_table.php
@@ -15,7 +15,18 @@ return new class extends Migration
     {
         Schema::create('tickets', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->date('date');
+            $table->text('notes');
+            $table->bigInteger('type_id')->unsigned();
+            // $table->foreignId('ticket_type_id');
+            // $table->foreign('type_id')->references('id')->on('ticket_types');
+            // $table->foreign('type_id')->references('id')->on('ticket_types')->onDelete('cascade');
+            // $table->foreignId('user_id')->constrained();
+            // $table->foreignId('type_id')->constrained('ticket_types');
+        });
+
+        Schema::table('tickets', function($table) {
+            $table->foreign('type_id')->references('id')->on('ticket_types');
         });
     }
 
